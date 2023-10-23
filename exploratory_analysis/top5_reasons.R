26398e3dcf1e474a915f1046306728310e521571
Yumei Yang <yumeiy@unc.edu.com>
2023-10-23 20:18:29 +0000

    produced two pie charts about the top 5 intake species besides cats and dogs, and the top 5 intake reasons

---
+++ exploratory_analysis/top5_reasons.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+data <- read_csv("project/derived_data/derived_data.csv")
+
+
+data <- data |>
+  group_by(
+    intakereason_new
+  ) |>
+  summarise(
+    n = n()
+  ) |>
+  mutate(
+    pct = 100*n/sum(n),
+    pct_label = paste0(round(pct, 1), "%")
+  ) |>
+  arrange(
+    desc(n)
+  ) 
+
+data$intakereason_new <- factor(data$intakereason_new, levels = as.character(data$intakereason_new))
+
+plot <- ggplot(data, aes(x = "", y = n, fill = intakereason_new)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) + 
+  geom_text(aes(x = 1.25, label = pct_label), position = position_stack(vjust = .5)) +
+  theme_void() +
+  guides(fill = guide_legend(title = "Intake Reasons"))
+
+ggsave("project/figures/top5_reasons.png", plot, width=20, height=15, units='cm')
