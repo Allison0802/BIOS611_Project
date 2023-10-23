586cf25bc1ff07bfeaca7192c5d98ca323cf6c3f
Yumei Yang <yumeiy@unc.edu.com>
2023-10-23 20:16:31 +0000

    created new species and intake reason variables for the figures

---
+++ data_cleaning.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+data <- read_csv(("project/data/Animal_Shelter_Animals.csv"))
+
+# get the top 5 intake reasons
+reasons <- data |>
+  group_by(intakereason) |>
+  summarise(
+    n = n()
+  ) |>
+  arrange(
+    desc(n)
+  ) 
+
+top5_reason <- reasons$intakereason[1:5]
+
+# get the top 5 species other than cats and dogs
+species <- data |>
+  group_by(speciesname) |>
+  summarise(
+    n = n()
+  ) |>
+  arrange(
+    desc(n)
+  ) 
+
+top7_species <- species$speciesname[1:7]
+
+#new intake reasons and speciesname variables
+data <- data |>
+  mutate(
+    intakereason_new = if_else(
+      intakereason %in% top5_reason, intakereason, "Other"
+    ),
+    speciesname_new = if_else(
+      speciesname %in% top7_species, speciesname, "Other"
+    )
+  )
+
+write_csv(data, "project/derived_data/derived_data.csv")
